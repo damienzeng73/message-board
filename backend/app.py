a338e7df0b2d382663b423b5c7e0a856129847a0
damnee562 <damnee562@gmail.com>
2018-05-19 17:29:08 +0800

    Fix CORS problem

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'messages'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/messages-board'
+CORS(app)
 
 mongo = PyMongo(app)
 
