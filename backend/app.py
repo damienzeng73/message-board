0678b39ded76cea57840a93e39d83e6f077faa85
damnee562 <damnee562@gmail.com>
2018-05-19 17:26:47 +0800

    Handle add new message

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'messages'
@@ -9,6 +10,30 @@ mongo = PyMongo(app)
 
 @app.route('/getMessages', methods=['GET'])
 def get_messages():
+    messages = []
+    for message in mongo.db.messages.find():
+        dic = {}
+        dic['username'] = message['username']
+        dic['content'] = message['content']
+        dic['timestamp'] = message['timestamp']
+
+        messages.append(dic)
+
+    return jsonify(messages[::-1])
+
+
+@app.route('/addMessage', methods=['POST'])
+def add_message():
+    username = request.json['username']
+    message = request.json['message']
+
+    if username and message:
+        mongo.db.messages.insert({
+            'username': username,
+            'content': message,
+            'timestamp': datetime.now()
+        })
+
     messages = []
     for message in mongo.db.messages.find():
         dic = {}
--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -12,12 +12,27 @@ class App extends React.Component {
             message: '',
             messages: []
         }
+
+        this.handleOnChange = this.handleOnChange.bind(this)
+        this.handleOnSubmit = this.handleOnSubmit.bind(this)
     }
 
     componentDidMount() {
         axios.get('http://localhost:5000/getMessages')
             .then((res) => {
                 this.setState({ messages: res.data })
+            })
+    }
+
+    handleOnChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleOnSubmit(e) {
+        e.preventDefault()
+        axios.post('http://localhost:5000/addMessage', {username: this.state.username, message: this.state.message})
+            .then((res) => {
+                this.setState({ username: '', message: '', messages: res.data })
             })
     }
 
@@ -28,11 +43,12 @@ class App extends React.Component {
 
                 <Divider />
 
-                <Form>
+                <Form onSubmit={this.handleOnSubmit}>
                     <Form.Input
                         name='username'
                         label='Username'
                         value={this.state.username}
+                        onChange={this.handleOnChange}
                     />
 
                     <Form.TextArea
@@ -40,6 +56,7 @@ class App extends React.Component {
                         label='Message'
                         placeholder='Leave some messages'
                         value={this.state.message}
+                        onChange={this.handleOnChange}
                     />
 
                     <Form.Button content='Submit' />
