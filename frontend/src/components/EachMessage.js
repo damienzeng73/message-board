edd9f00362e5567229af92ad9d0ea1b853a46d08
damnee562 <damnee562@gmail.com>
2018-05-19 18:40:41 +0800

    Style message list

--- frontend/src/components/EachMessage.js
+++ frontend/src/components/EachMessage.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 import { List } from 'semantic-ui-react'
 import _ from 'lodash'
 
+import './EachMessage.css'
+
 const EachMessage = (props) => {
     let messageCollection = _.map(props.messages, (element, index) => {
         return (
             <List.Item key={index}>
                 <List.Content>
                     <List.Header>{element.username}</List.Header>
-                    <List.Description>{element.content} - {element.timestamp}</List.Description>
+                    <List.Description>
+                        {element.content}
+                        <span className='timestamp'>{element.timestamp}</span>
+                    </List.Description>
                 </List.Content>
             </List.Item>
         )
